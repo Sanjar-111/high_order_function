98a5165ac0f9e38ee01af9e6cc3f6bcda29b53af
Sanjar-111 <dagarovsanjar38@gmail.com>
2026-05-21 11:04:42 +0500

    solved

--- filter_task_4_1.py
+++ filter_task_4_1.py
@@ -6,4 +6,6 @@ def filter_task_4_1(numbers):
     Returns:
         list of odd numbers
     """
-    return
+    return numbers%2
+numbers=[1, 2, 3, 4, 5]
+print(list(filter(filter_task_4_1,numbers)))
